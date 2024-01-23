52bb69c4662d01e0633a12dc1fa22e34962ebdfc
salah-koulal <salah.rajawi289@gmail.com>
2024-01-23 13:35:39 +0100

    Addsomecategories

--- frontend/src/assets/data/categories.js
+++ frontend/src/assets/data/categories.js
@@ -1,27 +1,38 @@
 export const categories = [
-    {
-      category: "Web Development",
-      desc: " related to designing and building websites or web applications.",
-      bgColor: "rgba(254, 182, 13, .2)",
-      textColor: "#FEB60D",
-    },
-    {
-      category: "Mobile App Development",
-      desc: " related to developing mobile applications for various platforms.",
-      bgColor: "rgba(151, 113, 255, .2)",
-      textColor: "#9771FF",
-    },
-    {
-      category: "UI/UX Design",
-      desc: " related to creating intuitive and visually appealing user interfaces.",
-      bgColor: "rgba(1, 181, 197, .2)",
-      textColor: "#01B5C5",
-    },
-    {
-      category: "Backend Development",
-      desc: " related to developing server-side logic and infrastructure for applications.",
-      bgColor: "rgba(1, 181, 197, .2)",
-      textColor: "#01B5C5",
-    },
-
-  ];
+  {
+    name: "Web Development",
+    desc: "Design and build dynamic websites and web applications.",
+    bgColor: "rgba(254, 182, 13, .2)",
+    textColor: "#FEB60D",
+  },
+  {
+    name: "Game Development",
+    desc: "Create interactive and immersive gaming experiences with game development technologies.",
+    bgColor: "rgba(108, 117, 125, .2)",
+    textColor: "#6C757D",
+  },
+  {
+    name: "Mobile App Development",
+    desc: "Craft mobile applications for iOS, Android, and cross-platform solutions.",
+    bgColor: "rgba(151, 113, 255, .2)",
+    textColor: "#9771FF",
+  },
+  {
+    name: "UI/UX Design",
+    desc: "Create visually stunning and user-friendly interfaces for exceptional user experiences.",
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+  },
+  {
+    name: "Backend Development",
+    desc: "Develop robust server-side logic and infrastructure to power applications.",
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+  },
+  {
+    name: "Frontend Development",
+    desc: "Build responsive and engaging user interfaces with cutting-edge frontend technologies.",
+    bgColor: "rgba(0, 123, 255, .2)",
+    textColor: "#007BFF",
+  },
+]
